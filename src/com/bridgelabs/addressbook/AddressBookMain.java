a76ff1237f5322ffa0d2638934baff72ed20d32b
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-06 22:39:10 +0530

    Added: Create a contact in addressBook functionality added

--- src/com/bridgelabs/addressbook/AddressBookMain.java
+++ src/com/bridgelabs/addressbook/AddressBookMain.java
@@ -1,10 +1,56 @@
 package com.bridgelabs.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
+
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// welcome message
 		System.out.println("Welcome to AddressBook System!");
+
+		AddressBookMain addressBookMain = new AddressBookMain();
+
+		// creates contact
+		Contact contact = addressBookMain.createContact();
+		// displays contact
+		addressBookMain.displayContact(contact);
 	}
 
+	private Contact createContact() {
+		System.out.println("Create a contact");
+		System.out.println("Enter first name:");
+		String firstName = sc.nextLine();
+		System.out.println("Enter last name:");
+		String lastName = sc.nextLine();
+		System.out.println("Enter address:");
+		String address = sc.nextLine();
+		System.out.println("Enter city:");
+		String city = sc.nextLine();
+		System.out.println("Enter state:");
+		String state = sc.nextLine();
+		System.out.println("Enter zip code:");
+		int zipCode = sc.nextInt();
+		System.out.println("Enter phone number:");
+		long phoneNo = sc.nextLong();
+		sc.nextLine();
+		System.out.println("Enter email:");
+		String email = sc.nextLine();
+
+		Contact contact = new Contact(firstName, lastName, address, city, state, zipCode, phoneNo, email);
+		return contact;
+	}
+
+	private void displayContact(Contact contact) {
+		System.out.println("Details of created contact: ");
+		System.out.println("First Name: " + contact.getFirstName());
+		System.out.println("Last Name: " + contact.getLastName());
+		System.out.println("Address: " + contact.getAddress());
+		System.out.println("City: " + contact.getCity());
+		System.out.println("State: " + contact.getState());
+		System.out.println("Zip Code: " + contact.getZipCode());
+		System.out.println("Phone Number: " + contact.getPhoneNo());
+		System.out.println("Email: " + contact.getEmail());
+	}
 }
---
+++ src/com/bridgelabs/addressbook/Contact.java
@@ -0,0 +1,91 @@
+package com.bridgelabs.addressbook;
+
+public class Contact {
+	// variables
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zipCode;
+	private long phoneNo;
+	private String email;
+
+	// parameter constructor
+	public Contact(String firstName, String lastName, String address, String city, String state, int zipCode,
+			long phoneNo, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+
+	// getters and setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
