c46759d3386c71d2f22848a6bbcda323d0d821d8
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-10-06 22:34:40 +0530

    Added: Welcome message added

---
+++ src/com/bridgelabs/addressbook/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabs.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// welcome message
+		System.out.println("Welcome to AddressBook System!");
+	}
+
+}
